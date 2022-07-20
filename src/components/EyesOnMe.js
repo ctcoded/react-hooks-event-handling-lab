21d6a3ef033bd3e119db46610530fbb46f9ef40f
cesartidrick <cesartidrick@gmail.com>
2022-07-20 09:09:48 -0600

    commit tree a134510d3a208da0bc36e042b9df50b68ce79893

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    console.log('in eyes on me')
+
+    const handleFocus = () => {
+        console.log("'Good!'")
+    }
+
+    const handleBlur = () => {
+        console.log("'Hey! Eyes on me!'")
+    }
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on Me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    console.log('inside keypad')
+
+    function handleChange(e) {
+        console.log("'Entering Password'")
+    } 
+
+    return (
+        <input type="password" onChange={handleChange}></input>
+    )
+}
+
+export default Keypad;
